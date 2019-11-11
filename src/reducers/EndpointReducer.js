61a46294290ea9d150d0632ff48247132d51a827
JackDiSalvatore <jdisalvatore7@gmail.com>
2019-11-10 20:13:42 -0600

    switching to jungle history endpoint

--- src/reducers/EndpointReducer.js
+++ src/reducers/EndpointReducer.js
@@ -6,7 +6,9 @@
 // const mainNetHistory = 'https://eos.greymass.com:443';
 // const mainNetBackup = 'http://api.cypherglass.com:8888';
 
-const initialState = 'https://eos.greymass.com:443' // Mainnet history
+// const initialState = 'https://eos.greymass.com:443' // Mainnet history
+// Replace this with a /v4 fetch using http://jungle.eosn.io/v2/docs/index.html
+const initialState = 'https://jungle.eosusa.news' // Jungle history
   
 export default function(state = initialState, action) {
   switch(action.type) {
